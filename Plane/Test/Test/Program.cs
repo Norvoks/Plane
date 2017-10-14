afbc73857c33ae314b8c5b284b74062317805a7d
Norvoks <32790289+Norvoks@users.noreply.github.com>
2017-10-14 13:22:42 +0300

    Add files via upload

---
+++ Plane/Test/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirCompany;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane.MaxAltitudeAuto = 10000;
+            Airplane.MinAltitudeAuto = 2000;
+            Airplane AN2=new Airplane(10,12,30);
+
+
+            AN2.Climb(200);
+            Airplane.SetAltitude(3000);
+
+        }
+    }
+}
